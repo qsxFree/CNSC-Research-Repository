3e2fb8a383a59e0ed072c1a5e81aaac76035d6fa
Ralph Royeen Lagumen <mstream64@gmail.com>
2021-06-30 15:05:31 +0800

    initial commit

---
+++ src/main/java/com/cnsc/research/ResearchRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.cnsc.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cnsc/research/ResearchRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnsc.research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
