0b143aa9a169bbed314258b76b9ff517141b40c9
Ralph Royeen Lagumen <mstream64@gmail.com>
2022-02-09 23:04:39 +0800

    adding more entity for future changes

--- src/main/java/com/cnsc/research/domain/model/EntityType.java
+++ src/main/java/com/cnsc/research/domain/model/EntityType.java
@@ -3,5 +3,10 @@ package com.cnsc.research.domain.model;
 public enum EntityType {
     PRESENTATION,
     RESEARCH,
-    PUBLICATION
+    PUBLICATION,
+
+    RESEARCHER,
+    DELIVERY_UNIT,
+    FUNDING_AGENCY,
+    AGENDA
 }
